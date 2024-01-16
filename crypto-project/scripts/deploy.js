2b4597270efe98eb2fa1f8833ef465820900da2c
Alice Khovrus <alice.khovrus@gmail.com>
2024-01-16 22:42:48 +0800

    test (change a lil bit later)

--- crypto-project/scripts/deploy.js
+++ crypto-project/scripts/deploy.js
@@ -1,10 +1,3 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-
 import pkg from "hardhat";
 const { ethers } = pkg;
 
@@ -24,19 +17,6 @@ async function main() {
   );
   await accountManager.deployed();
   console.log("AccountManager deployed to:", await accountManager.getAddress());
-
-  const fundrisingDuration = 24 * 60 * 60;
-  const timeForTraiding = 30 * 24 * 60 * 60;
-  const LiquidityPool = await ethers.getContractFactory("LiquidityPool");
-  const liquidityPool = await LiquidityPool.deploy(
-    manager.address,
-    usdcAddress,
-    fundrisingDuration,
-    timeForTraiding,
-    traidingAccAddress
-  );
-  await liquidityPool.deployed();
-  console.log("LiquidityPool deployed to:", await liquidityPool.getAddress());
 }
 
 main().catch((error) => {
