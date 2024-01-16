44b254e2facfdea07cb09c4bee11eea042130bb6
Aleksandra Gontar <a.k.gontar02@mail.ru>
2024-01-16 14:58:53 +0800

    hehe

--- crypto-project/scripts/deploy.js
+++ crypto-project/scripts/deploy.js
@@ -1,31 +1,7 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-
 import pkg from "hardhat";
 const { ethers } = pkg;
 
 async function main() {
-  //   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  //   const unlockTime = currentTimestampInSeconds + 60;
-
-  //   const lockedAmount = hre.ethers.parseEther("0.001");
-
-  //   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-  //     value: lockedAmount,
-  //   });
-
-  //   await lock.waitForDeployment();
-
-  //   console.log(
-  //     `Lock with ${ethers.formatEther(
-  //       lockedAmount
-  //     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  //   );
-  // }
   const [owner, manager, client] = await ethers.getSigners();
   const USDC = await ethers.getContractFactory("USDC");
   const usdc = await USDC.deploy();
@@ -38,7 +14,6 @@ async function main() {
     usdcAddress,
     traidingAccAddress
   );
-  await accountManager.deployed();
   console.log("AccountManager deployed to:", await accountManager.getAddress());
 
   const traidingTime = 24 * 60 * 60;
@@ -50,7 +25,6 @@ async function main() {
     traidingAccAddress
   );
   // await usdc.transfer(client.address, 1_000_000e6);
-  await liquidityPool.deployed();
   console.log("LiquidityPool deployed to:", await liquidityPool.getAddress());
 }
 
